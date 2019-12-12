5a2295183d63f918033a367e1638a3b1c2dcfea2
BraviMathieu <44873477+bravimathieu@users.noreply.github.com>
2019-12-12 08:49:27 +0100

    MAJ

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -13,52 +13,32 @@
       <div class="wrap-register">
         <form id="register-form" class="register-form validate-form">
           <h1 class="register-form-title">Créer un compte</h1>
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="wrap-input">
-                <span class="label-input">Nom</span>
-                <input class="input" type="text" name="nom"
-                       v-model="inputNom" placeholder="Nom...">
-                <span class="focus-input"></span>
-              </div>
+            <div class="wrap-input">
+              <span class="label-input">Nom</span>
+              <input class="input" type="text" name="nom"
+                     v-model="inputNom" placeholder="Nom...">
+              <span class="focus-input"></span>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="wrap-input">
-                <span class="label-input">Prénom</span>
-                <input class="input" type="text" name="prenom"
-                       v-model="inputPrenom" placeholder="Prénom...">
-                <span class="focus-input"></span>
-              </div>
+            <div class="wrap-input">
+              <span class="label-input">Prénom</span>
+              <input class="input" type="text" name="prenom"
+                     v-model="inputPrenom" placeholder="Prénom...">
+              <span class="focus-input"></span>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="wrap-input">
-                <span class="label-input">E-mail</span>
-                <input class="input" type="email" name="email"
-                       v-model="inputEmail" placeholder="E-mail...">
-                <span class="focus-input"></span>
-              </div>
+            <div class="wrap-input">
+              <span class="label-input">E-mail</span>
+              <input class="input" type="email" name="email"
+                     v-model="inputEmail" placeholder="E-mail...">
+              <span class="focus-input"></span>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="wrap-input">
-                <span class="label-input">Mot de passe</span>
-                <input class="input" type="password" name="mdp"
-                       v-model="inputMdp" placeholder="Mot de passe...">
-                <span class="focus-input"></span>
-              </div>
+            <div class="wrap-input">
+              <span class="label-input">Mot de passe</span>
+              <input class="input" type="password" name="mdp"
+                     v-model="inputMdp" placeholder="Mot de passe...">
+              <span class="focus-input"></span>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-lg-6 offset-lg-3">
-              <input id="submit-btn" type="button" class="register-form-btn" @click="register()"
-                     value="Créer un compte">
-            </div>
-          </div>
+            <input id="submit-btn" type="button" class="register-form-btn" @click="register()"
+                   value="Créer un compte">
         </form>
       </div>
     </div>
@@ -121,74 +101,92 @@ export default {
 </script>
 
 <style scoped>
-  @font-face {
-    font-family: Poppins-Regular;
-    src: url('../../public/fonts/poppins/Poppins-Regular.ttf');
-  }
 
-  @font-face {
-    font-family: Poppins-Medium;
-    src: url('../../public/fonts/poppins/Poppins-Medium.ttf');
-  }
-
-  @font-face {
-    font-family: Poppins-Bold;
-    src: url('../../public/fonts/poppins/Poppins-Bold.ttf');
-  }
-
-  @font-face {
-    font-family: Poppins-SemiBold;
-    src: url('../../public/fonts/poppins/Poppins-SemiBold.ttf');
-  }<style scoped>
-  @font-face {
-    font-family: Poppins-Regular;
-    src: url('../../public/fonts/poppins/Poppins-Regular.ttf');
-  }
-
-  @font-face {
-    font-family: Poppins-Medium;
-    src: url('../../public/fonts/poppins/Poppins-Medium.ttf');
-  }
-
-  @font-face {
-    font-family: Poppins-Bold;
-    src: url('../../public/fonts/poppins/Poppins-Bold.ttf');
-  }
-
-  @font-face {
-    font-family: Poppins-SemiBold;
-    src: url('../../public/fonts/poppins/Poppins-SemiBold.ttf');
-  }
-
+  /*---------------------------------------------*/
   a {
     font-family: Poppins-Regular;
     font-size: 14px;
+    line-height: 1.7;
+    color: #666666;
+    margin: 0px;
     transition: all 0.4s;
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
   }
+
+  a:focus {
+    outline: none !important;
+  }
+
   a:hover {
     text-decoration: none;
   }
 
+  /*---------------------------------------------*/
+
   p {
     font-family: Poppins-Regular;
     font-size: 14px;
+    line-height: 1.7;
+    color: #666666;
+    margin: 0px;
   }
 
+  ul, li {
+    margin: 0px;
+    list-style-type: none;
+  }
+
+  /*---------------------------------------------*/
   input {
+    outline: none;
     border: none;
   }
 
+  input:focus::-webkit-input-placeholder { color:transparent; }
+  input:focus:-moz-placeholder { color:transparent; }
+  input:focus::-moz-placeholder { color:transparent; }
+  input:focus:-ms-input-placeholder { color:transparent; }
+
+  textarea:focus::-webkit-input-placeholder { color:transparent; }
+  textarea:focus:-moz-placeholder { color:transparent; }
+  textarea:focus::-moz-placeholder { color:transparent; }
+  textarea:focus:-ms-input-placeholder { color:transparent; }
+
+  input::-webkit-input-placeholder {color: #cccccc;}
+  input:-moz-placeholder {color: #cccccc;}
+  input::-moz-placeholder {color: #cccccc;}
+  input:-ms-input-placeholder {color: #cccccc;}
+
+  textarea::-webkit-input-placeholder {color: #cccccc;}
+  textarea:-moz-placeholder {color: #cccccc;}
+  textarea::-moz-placeholder {color: #cccccc;}
+  textarea:-ms-input-placeholder {color: #cccccc;}
+
+  /*---------------------------------------------*/
   button {
+    outline: none !important;
     border: none;
     background: transparent;
   }
+
   button:hover {
     cursor: pointer;
   }
 
+  iframe {
+    border: none !important;
+  }
+
+  /*//////////[ login ]*/
 
   .container-register {
     width: 100%;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
     display: flex;
     flex-wrap: wrap;
     align-items: stretch;
@@ -202,10 +200,13 @@ export default {
     border-radius: 2px;
     position: relative;
     padding: 40px;
+    text-align: left;
   }
 
+  /*------------------------------------------------------------------
+  [ Login100 more ]*/
   .register-more {
-    background-image: url('../bg-02.png');
+    background-image: url('../bg-01.png');
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
@@ -224,12 +225,22 @@ export default {
     top: 0;
     left: 0;
     background: #006CFF;
+    background: -webkit-linear-gradient(bottom, #006CFF, #0031ff);
+    background: -o-linear-gradient(bottom, #006CFF, #0031ff);
+    background: -moz-linear-gradient(bottom, #006CFF, #0031ff);
     background: linear-gradient(bottom, #006CFF, #0031ff);
     opacity: 0.8;
   }
 
+  /*==================================================================
+  [ Form ]*/
+
   .register-form {
     width: 100%;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
@@ -241,22 +252,27 @@ export default {
     font-family: Poppins-Bold;
     font-size: 39px;
     color: #333333;
+    line-height: 1.2;
     text-align: center;
     padding-bottom: 59px;
   }
+
+
+  /*------------------------------------------------------------------
+  [ Input ]*/
 
   .wrap-input {
     width: 100%;
     position: relative;
     border-bottom: 2px solid #dbdbdb;
     margin-bottom: 45px;
-    text-align: left;
   }
 
   .label-input {
     font-family: Poppins-SemiBold;
     font-size: 18px;
-    color: #0031ff;
+    color: #999999;
+    line-height: 1.2;
     padding-left: 2px;
   }
 
@@ -268,6 +284,7 @@ export default {
     font-family: Poppins-Regular;
     font-size: 22px;
     color: #555555;
+    line-height: 1.2;
     padding: 0 2px;
   }
 
@@ -289,9 +306,17 @@ export default {
     left: 0;
     width: 0;
     height: 2px;
+
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
     transition: all 0.4s;
-    background: #006CFF;
-    background: linear-gradient(45deg, #006CFF, #525252);
+
+    background: #0031ff;
+    background: -webkit-linear-gradient(45deg, #0031ff, #525252);
+    background: -o-linear-gradient(45deg, #0031ff, #525252);
+    background: -moz-linear-gradient(45deg, #0031ff, #525252);
+    background: linear-gradient(45deg, #0031ff, #525252);
   }
 
   .input:focus + .focus-input::before {
@@ -302,33 +327,47 @@ export default {
     width: 100%;
   }
 
+  .input:focus {
+    width: 100%;
+  }
+
+  /*------------------------------------------------------------------
+  [ Button ]*/
+
 
   .register-form-btn {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     padding: 0 20px;
+    min-width: 244px;
     height: 50px;
+
     font-family: Poppins-Medium;
     font-size: 16px;
     color: #fff;
+    line-height: 1.2;
     border-radius: 25px;
     background:#006CFF;
   }
-
   .register-form-btn:hover {
     background: #525252;
     box-shadow: inset 0 0 10px #000000;
     cursor: pointer;
   }
 
-  .wrap-pic-cir img {
-    width: 100%;
+  /*//////////////////////////////////////////////////////////////////
+  [ Responsive ]*/
+
+  @media (max-width: 576px) {
+    .wrap-register {
+      padding-left: 15px;
+      padding-right: 15px;
+    }
   }
 
-  .hov-img-zoom img{
-    width: 100%;
-    transition: all 0.6s;
-  }
-
-  .hov-img-zoom:hover img {
-    transform: scale(1.1);
-  }
-  </style>
+</style>
