abf6db6daaf33d47b7aceecc858c81e600a12651
kyllian1212 <pelletier.kyllian@gmail.com>
2020-02-20 11:15:47 +0100

    partie paiement

--- src/views/Payer.vue
+++ src/views/Payer.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="container">
-        <form id="payment-form">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-xl-5 col-lg-6 col-md-7 col-sm-9 col-12">
+        <br>
+        <h1>Paiement</h1>
+        <p>Veuillez renseigner vos coordonées bancaires.</p>
+        <form id="payment-form" @load="stripeLaunch()">
           <div id="card-element">
             <!--Elements will create input elements here-->
           </div>
-
+          <br>
           <!--We'll put the error messages in this element-->
           <div id="card-errors" role="alert"></div>
           <div id="card-success" role="alert"></div>
-          <button id="submit">Pay</button>
+          <br>
+          <button id="submit" class="btn btn-outline-primary">Payer</button>
         </form>
       </div>
     </div>
@@ -18,64 +22,75 @@
 </template>
 
 <script>
-function stripeLaunch() {
-  // Set your publishable key: remember to change this to your live publishable key in production
-  // See your keys here: https://dashboard.stripe.com/account/apikeys
-  // eslint-disable-next-line no-undef
-  const stripe = Stripe('pk_test_RaklY8fqgQueX56HMWvoKKmI00XAWwZhah');
-  const elements = stripe.elements();
-  // Set up Stripe.js and Elements to use in checkout form
-  const style = {
-    base: {
-      color: '#32325d',
-    },
-  };
-
-  const card = elements.create('card', { style });
-  card.mount('#card-element');
-
-  card.addEventListener('change', ({ error }) => {
-    const displayError = document.getElementById('card-errors');
-    if (error) {
-      displayError.textContent = error.message;
-    } else {
-      displayError.textContent = '';
-    }
-  });
-
-  const form = document.getElementById('payment-form');
-  const clientSecret = 'pi_1GDYsKA8vJqX70O5fsKKlK8S_secret_nq6LUxZ2Asda9CIGAMb05lK4h'; // a changer depuis le serveur pour le moment a chaque requête
-  form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    // eslint-disable-next-line no-undef
-    stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card,
-        billing_details: {
-          name: 'Jenny Rosen',
-        },
-      },
-    }).then((result) => {
-      if (result.error) {
-        document.getElementById('card-errors').innerText = 'Il y\'a eu un souci avec votre paiement. Veuillez réessayer plus tard ou modifiez vos coordonnées.';
-        console.log(result.error.message);
-      } else {
-        // The payment has been processed!
-        // eslint-disable-next-line no-lonely-if
-        if (result.paymentIntent.status === 'succeeded') {
-          document.getElementById('submit').disabled = true;
-          document.getElementById('card-errors').remove();
-          document.getElementById('card-success').innerText = 'Paiement réussi!';
-        }
-      }
-    });
-  });
-}
-
-setTimeout(() => { stripeLaunch(); }, 0);
-
 export default {
   name: 'payer',
+  mounted() {
+    this.stripeLaunch();
+  },
+  methods: {
+    stripeLaunch() {
+      // eslint-disable-next-line max-len
+      // Set your publishable key: remember to change this to your live publishable key in production
+      // See your keys here: https://dashboard.stripe.com/account/apikeys
+      // eslint-disable-next-line no-undef
+      const stripe = Stripe('pk_test_RaklY8fqgQueX56HMWvoKKmI00XAWwZhah');
+      const elements = stripe.elements();
+      // Set up Stripe.js and Elements to use in checkout form
+      const style = {
+        base: {
+          color: '#32325d',
+        },
+      };
+
+      const card = elements.create('card', { style });
+      card.mount('#card-element');
+
+      card.addEventListener('change', ({ error }) => {
+        const displayError = document.getElementById('card-errors');
+        if (error) {
+          displayError.textContent = error.message;
+        } else {
+          displayError.textContent = '';
+        }
+      });
+
+      const form = document.getElementById('payment-form');
+      let clientSecret;
+      const promise = fetch('http://localhost:3000/secret')
+        .then(res => res.text());
+      promise.then((data) => {
+        clientSecret = data;
+      });
+      form.addEventListener('submit', (ev) => {
+        ev.preventDefault();
+        // eslint-disable-next-line no-undef
+        stripe.confirmCardPayment(clientSecret, {
+          payment_method: {
+            card,
+            billing_details: {
+              name: 'Jenny Rosen',
+            },
+          },
+        })
+          .then((result) => {
+            if (result.error) {
+              document.getElementById('card-errors').innerText = 'Il y\'a eu un souci avec votre paiement. Veuillez réessayer plus tard ou modifiez vos coordonnées.';
+              console.log(result.error.message);
+            } else {
+              // The payment has been processed!
+              // eslint-disable-next-line no-lonely-if
+              if (result.paymentIntent.status === 'succeeded') {
+                document.getElementById('submit').hidden = true;
+                document.getElementById('card-errors')
+                  .remove();
+                document.getElementById('card-success').innerText = 'Paiement réussi!';
+                setTimeout(() => { this.$router.push({ path: 'paiementreussi' }); }, 1250);
+              }
+            }
+          });
+      });
+    },
+  },
 };
 </script>
 
