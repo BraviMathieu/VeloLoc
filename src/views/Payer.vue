bb021b4209b011e728aaaf951d694434a86beb0a
kyllian1212 <pelletier.kyllian@gmail.com>
2020-02-20 12:25:40 +0100

    partie paiement

--- src/views/Payer.vue
+++ src/views/Payer.vue
@@ -13,14 +13,17 @@
           <!--We'll put the error messages in this element-->
           <div id="card-errors" role="alert"></div>
           <div id="card-success" role="alert"></div>
+          <div id="redir" role="alert"></div>
           <br>
           <button id="submit" class="btn btn-outline-primary">Payer</button>
+          <br>
+          <br>
+          <button id="panier" class="btn btn-outline-secondary">Retour au panier</button>
         </form>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'payer',
@@ -74,17 +77,39 @@ export default {
         })
           .then((result) => {
             if (result.error) {
-              document.getElementById('card-errors').innerText = 'Il y\'a eu un souci avec votre paiement. Veuillez réessayer plus tard ou modifiez vos coordonnées.';
+              const err = result.error.message;
+              document.getElementById('card-errors').innerText = err;
               console.log(result.error.message);
+              if (err.includes('Votre carte a été refusée.')
+                || err.includes('Votre carte ne dispose pas de fonds suffisants.')) {
+                let messageDuration = err.length * 80;
+                // eslint-disable-next-line max-len
+                if (messageDuration > 8000) { messageDuration = 8000; } else if (messageDuration < 2500) { messageDuration = 2500; }
+                console.log(messageDuration);
+                this.$notify({
+                  group: 'veloloc',
+                  componentName: 'submit',
+                  title: 'Transaction refusée',
+                  type: 'error',
+                  duration: messageDuration,
+                  text: `${result.error.message} <br> Redirection dans ${(messageDuration / 1000).toFixed(0)} secondes...`,
+                  position: 'top center',
+                });
+                document.getElementById('submit').disabled = true;
+                document.getElementById('panier').disabled = true;
+                setTimeout(() => { this.$router.push({ path: 'paiementrate' }); }, messageDuration);
+              }
             } else {
               // The payment has been processed!
               // eslint-disable-next-line no-lonely-if
               if (result.paymentIntent.status === 'succeeded') {
-                document.getElementById('submit').hidden = true;
+                document.getElementById('submit').disabled = true;
+                document.getElementById('panier').disabled = true;
                 document.getElementById('card-errors')
                   .remove();
                 document.getElementById('card-success').innerText = 'Paiement réussi!';
-                setTimeout(() => { this.$router.push({ path: 'paiementreussi' }); }, 1250);
+                document.getElementById('redir').innerText = 'Redirection...';
+                setTimeout(() => { this.$router.push({ path: 'paiementreussi' }); }, 2000);
               }
             }
           });
