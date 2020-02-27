4041496ef13edb05f5ee832515602b6276a20f78
BraviMathieu <mathieu.bravi@free.fr>
2020-02-27 19:30:19 +0100

    Modif Css et fond

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,195 @@
   /*//////////////////////////////////////////////////////////////////
   [ FONT ]*/
 
+  html, body{
+    background: url("./bg.svg");
+    background-size: cover;
+    background-position-x: 20vw;
+    background-position-y: -10vh;
+    background-repeat: no-repeat
+  }
+
+  .darkmode--activated h1, .darkmode--activated li {
+    color: #ffffff !important;
+  }
+
+  .darkmode--activated span{
+    color: #dedede !important;
+  }
+
+  .darkmode--activated .input{
+    background-color: #000000 !important;
+  }
+
+  .darkmode-toggle{
+    z-index: 100000;
+  }
+
+  .jumbotron{
+    background-color: #f6f8fa;
+  }
+
+
+  p {
+    font-family: Poppins-Regular;
+    font-size: 14px;
+    line-height: 1.7;
+    color: #666666;
+    margin: 0px;
+  }
+
+  a {
+    font-family: Poppins-Regular;
+    font-size: 14px;
+    line-height: 1.7;
+    color: #666666;
+    margin: 0px;
+    transition: all 0.4s;
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
+  }
+
+
+  a:focus {
+    outline: none !important;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+
+  ul, li {
+    margin: 0px;
+    list-style-type: none;
+  }
+
+  input {
+    outline: none;
+    border: none;
+  }
+
+  /*---------------------------------------------*/
+
+  input:focus::-webkit-input-placeholder { color:transparent; }
+  input:focus:-moz-placeholder { color:transparent; }
+  input:focus::-moz-placeholder { color:transparent; }
+  input:focus:-ms-input-placeholder { color:transparent; }
+
+  textarea:focus::-webkit-input-placeholder { color:transparent; }
+  textarea:focus:-moz-placeholder { color:transparent; }
+  textarea:focus::-moz-placeholder { color:transparent; }
+  textarea:focus:-ms-input-placeholder { color:transparent; }
+
+  /*---------------------------------------------*/
+
+  button {
+    outline: none !important;
+    border: none;
+    background: transparent;
+  }
+
+  /*---------------------------------------------*/
+
+
+  .container-modify {
+    width: 100%;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+    flex-wrap: wrap;
+  }
+
+  .wrap-modify {
+    width: 520px;
+    min-height: 100vh;
+    border-radius: 2px;
+    position: relative;
+    padding: 40px;
+    text-align: left;
+    width: 1200px;
+  }
+
+  /*---------------------------------------------*/
+
+  .input {
+    display: block;
+    width: 100%;
+    height: 50px;
+    background: white;
+    font-family: Poppins-Regular;
+    font-size: 22px;
+    color: #555555;
+    line-height: 1.2;
+    padding: 0 2px;
+
+  }
+
+  .focus-input {
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+  }
+
+  .focus-input::before {
+    content: "";
+    display: block;
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    width: 0;
+    height: 2px;
+
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
+    transition: all 0.4s;
+
+    background: #0031ff;
+    background: -webkit-linear-gradient(45deg, #0031ff, #525252);
+    background: -o-linear-gradient(45deg, #0031ff, #525252);
+    background: -moz-linear-gradient(45deg, #0031ff, #525252);
+    background: linear-gradient(45deg, #0031ff, #525252);
+  }
+
+  .input:focus + .focus-input::before {
+    width: 100%;
+  }
+
+  .has-val.input + .focus-input::before {
+    width: 100%;
+  }
+
+  .input:focus {
+    width: 100%;
+  }
+
+
+  .wrap-input {
+    width: 100%;
+    position: relative;
+    border-bottom: 2px solid #dbdbdb;
+    margin-bottom: 45px;
+  }
+
+
+  .label-input {
+    font-family: Poppins-SemiBold;
+    font-size: 18px;
+    color: #6b6b6b;
+    line-height: 1.2;
+    padding-left: 2px;
+  }
+
   @font-face {
     font-family: Poppins-Regular;
     src: url('../public/fonts/poppins/Poppins-Regular.ttf');
@@ -69,4 +258,15 @@
     }
   }
 }
+
+  /*//////////////////////////////////////////////////////////////////
+[ Responsive ]*/
+
+  @media (max-width: 576px) {
+    .wrap-modify {
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+  }
+
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -37,8 +37,11 @@
                      v-model="inputMdpConfirmation" placeholder="Mot de passe...">
               <span class="focus-input"></span>
             </div>
-            <input id="submit-btn" type="button" class="register-form-btn" @click="register()"
-                   value="Créer un compte">
+        </div>
+        <div class="text-center">
+          <input id="submit-btn" type="button" class="register-form-btn"
+                 @click="register()"
+                 value="Créer un compte">
         </div>
       </div>
     </div>
@@ -119,84 +122,8 @@ export default {
 
 <style scoped>
 
-  /*---------------------------------------------*/
-  a {
-    font-family: Poppins-Regular;
-    font-size: 14px;
-    line-height: 1.7;
-    color: #666666;
-    margin: 0px;
-    transition: all 0.4s;
-    -webkit-transition: all 0.4s;
-    -o-transition: all 0.4s;
-    -moz-transition: all 0.4s;
-  }
 
-  a:focus {
-    outline: none !important;
-  }
-
-  a:hover {
-    text-decoration: none;
-  }
-
-  /*---------------------------------------------*/
-
-  p {
-    font-family: Poppins-Regular;
-    font-size: 14px;
-    line-height: 1.7;
-    color: #666666;
-    margin: 0px;
-  }
-
-  ul, li {
-    margin: 0px;
-    list-style-type: none;
-  }
-
-  /*---------------------------------------------*/
-  input {
-    outline: none;
-    border: none;
-  }
-
-  input:focus::-webkit-input-placeholder { color:transparent; }
-  input:focus:-moz-placeholder { color:transparent; }
-  input:focus::-moz-placeholder { color:transparent; }
-  input:focus:-ms-input-placeholder { color:transparent; }
-
-  textarea:focus::-webkit-input-placeholder { color:transparent; }
-  textarea:focus:-moz-placeholder { color:transparent; }
-  textarea:focus::-moz-placeholder { color:transparent; }
-  textarea:focus:-ms-input-placeholder { color:transparent; }
-
-  input::-webkit-input-placeholder {color: #cccccc;}
-  input:-moz-placeholder {color: #cccccc;}
-  input::-moz-placeholder {color: #cccccc;}
-  input:-ms-input-placeholder {color: #cccccc;}
-
-  textarea::-webkit-input-placeholder {color: #cccccc;}
-  textarea:-moz-placeholder {color: #cccccc;}
-  textarea::-moz-placeholder {color: #cccccc;}
-  textarea:-ms-input-placeholder {color: #cccccc;}
-
-  /*---------------------------------------------*/
-  button {
-    outline: none !important;
-    border: none;
-    background: transparent;
-  }
-
-  button:hover {
-    cursor: pointer;
-  }
-
-  iframe {
-    border: none !important;
-  }
-
-  /*//////////[ login ]*/
+  /*//////////[ register ]*/
 
   .container-modify {
     width: 100%;
@@ -213,7 +140,6 @@ export default {
   .wrap-modify {
     width: 520px;
     min-height: 100vh;
-    background: #fff;
     border-radius: 2px;
     position: relative;
     padding: 40px;
@@ -276,87 +202,9 @@ export default {
 
 
   /*------------------------------------------------------------------
-  [ Input ]*/
-
-  .wrap-input {
-    width: 100%;
-    position: relative;
-    border-bottom: 2px solid #dbdbdb;
-    margin-bottom: 45px;
-  }
-
-  .label-input {
-    font-family: Poppins-SemiBold;
-    font-size: 18px;
-    color: #999999;
-    line-height: 1.2;
-    padding-left: 2px;
-  }
-
-  .input {
-    display: block;
-    width: 100%;
-    height: 50px;
-    background: transparent;
-    font-family: Poppins-Regular;
-    font-size: 22px;
-    color: #555555;
-    line-height: 1.2;
-    padding: 0 2px;
-  }
-
-  .focus-input {
-    position: absolute;
-    display: block;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    pointer-events: none;
-  }
-
-  .focus-input::before {
-    content: "";
-    display: block;
-    position: absolute;
-    bottom: -2px;
-    left: 0;
-    width: 0;
-    height: 2px;
-
-    -webkit-transition: all 0.4s;
-    -o-transition: all 0.4s;
-    -moz-transition: all 0.4s;
-    transition: all 0.4s;
-
-    background: #0031ff;
-    background: -webkit-linear-gradient(45deg, #0031ff, #525252);
-    background: -o-linear-gradient(45deg, #0031ff, #525252);
-    background: -moz-linear-gradient(45deg, #0031ff, #525252);
-    background: linear-gradient(45deg, #0031ff, #525252);
-  }
-
-  .input:focus + .focus-input::before {
-    width: 100%;
-  }
-
-  .has-val.input + .focus-input::before {
-    width: 100%;
-  }
-
-  .input:focus {
-    width: 100%;
-  }
-
-  /*------------------------------------------------------------------
   [ Button ]*/
 
   .register-form-btn {
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -moz-box;
-    display: -ms-flexbox;
-    display: flex;
     justify-content: center;
     align-items: center;
     padding: 0 20px;
@@ -374,24 +222,6 @@ export default {
     background: #525252;
     box-shadow: inset 0 0 10px #000000;
     cursor: pointer;
-  }
-
-  /*//////////////////////////////////////////////////////////////////
-  [ Responsive ]*/
-
-  @media (max-width: 576px) {
-    .wrap-modify {
-      padding-left: 15px;
-      padding-right: 15px;
-    }
-  }
-
-
-  .monalert{
-    position: fixed;
-    z-index: 500;
-    text-align: center;
-
   }
 
 </style>
